7ee14d27d0fa4de2dd0856075ea1c1b1cd90b10c
ronaldsm <ronaldsm@cisco.com>
2019-03-08 15:19:24 -0700

    allow updates

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -17,6 +17,20 @@ class TestsController < ApplicationController
     redirect_to @test
   end
 
+  def edit
+    @test = Test.find(params[:id])
+  end
+
+  def update
+    @test = Test.find(params[:id])
+
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render 'edit'
+    end
+  end
+
   private
     def test_params
       params.require(:test).permit(:name, :priority, :persona, :description)
