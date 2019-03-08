0b7c2a82ddebf76e855e82ade670e6be47ba1daa
ronaldsm <ronaldsm@cisco.com>
2019-03-08 11:52:32 -0700

    Add first form

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,4 +1,7 @@
 class TestsController < ApplicationController
   def index
   end
+
+  def new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get 'tests/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root  'test#index'
+
+  resources :tests
 end
