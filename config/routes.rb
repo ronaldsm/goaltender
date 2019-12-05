312dfd32040f3ae1ae6465c41448bc36fa669547
ronaldsm <ronaldsm@cisco.com>
2019-12-04 21:14:12 -0700

    Add users with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :areas
   resources :cores
   resources :personas
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_16_175904) do
+ActiveRecord::Schema.define(version: 2019_12_05_040241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,13 +23,14 @@ ActiveRecord::Schema.define(version: 2019_05_16_175904) do
   end
 
   create_table "cores", force: :cascade do |t|
-    t.string "area"
     t.string "persona"
     t.string "name"
     t.text "short_description"
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "area_id"
+    t.index ["area_id"], name: "index_cores_on_area_id"
   end
 
   create_table "personas", force: :cascade do |t|
@@ -49,4 +50,17 @@ ActiveRecord::Schema.define(version: 2019_05_16_175904) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "cores", "areas"
 end
